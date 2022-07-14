561903c9a0c14c72525105f6bf10ac8a3f5d08ff
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-07-14 18:33:04 -0500

    [OneToOneField] Starting new app por users management

--- platzigram/platzigram/settings.py
+++ platzigram/platzigram/settings.py
@@ -42,6 +42,7 @@ DJANGO_APPS = [
 THIRD_APPS = []
 
 LOCAL_APPS = [
+    'users',
     'posts',
 ]
 
---
+++ platzigram/users/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    name = 'users'
+    verbose_name= 'Users' # this is not a default line
