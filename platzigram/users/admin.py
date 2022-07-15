b13cfe9bc8f152d78f5371442024c93fb1352d18
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-07-14 19:01:07 -0500

    [Admin models] Add model Profile to admin

--- platzigram/users/admin.py
+++ platzigram/users/admin.py
@@ -1,3 +1,26 @@
+# Django
 from django.contrib import admin
+# Models
+from .models import Profile
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    list_display = ('user', 'phone_number', 'website')
+    list_display_links = ('user',)
+    list_editable = ('phone_number',)
+
+    search_fields = (
+        'user__email',
+        'user__username',
+        'user__first_name',
+        'user__last_name',
+        'phone_number'
+    )
+
+    list_filter = (
+        'user__is_active',
+        'user__is_staff',
+        'created',
+        'modified',
+    )
