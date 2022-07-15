d0d5c8889b3f3b9b5af090fab72bb33118a5b521
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-07-15 17:37:50 -0500

    [Users] Add management to signup - based function

--- platzigram/users/views.py
+++ platzigram/users/views.py
@@ -2,6 +2,39 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+# Exceptions
+from django.db.utils import IntegrityError
+# Models
+from django.contrib.auth.models import User
+from .models import Profile
+
+# Views
+def signup_view(request):
+    """Sign up view."""
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirmation = request.POST['passwd_confirmation']
+
+        if passwd != passwd_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+
+        try:
+            user = User.objects.create_user(username=username, password=passwd)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already in user'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('users:login')
+
+    return render(request, 'users/signup.html')
 
 def login_view(request):
     """Login view."""
@@ -19,5 +52,6 @@ def login_view(request):
 
 @login_required
 def logout_view(request):
+    """Logout view."""
     logout(request)
     return redirect('users:login')
