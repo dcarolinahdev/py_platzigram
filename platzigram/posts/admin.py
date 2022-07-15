8b077192d98fbe1a0331d20514f8a2c335392b55
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-07-15 14:21:27 -0500

    [Posts] Add model Post to admin

--- platzigram/posts/admin.py
+++ platzigram/posts/admin.py
@@ -1,3 +1,39 @@
+# Django
 from django.contrib import admin
+# Models
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'modified')
+    list_display_links = ('user',)
+
+    search_fields = (
+        'user__email',
+        'user__username',
+        'user__first_name',
+        'user__last_name',
+        'title'
+    )
+
+    list_filter = (
+        'user__is_active',
+        'user__is_staff',
+        'modified',
+    )
+
+    fieldsets = (
+        ('User info', {
+            'fields': (('user', 'profile'),)
+        }),
+        ('Post info', {
+            'fields': (
+                ('title', 'photo'),
+            )
+        }),
+        ('Metadata', {
+            'fields': (('created', 'modified'),),
+        })
+    )
+
+    readonly_fields = ('created', 'modified',)
