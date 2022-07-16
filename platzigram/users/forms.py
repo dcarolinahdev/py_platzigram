0d8ca105150a9c55e74ff8ddc199bd1e2793c8cc
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-07-16 17:07:10 -0500

    [Users] Updating form and template for update profile function

--- platzigram/users/forms.py
+++ platzigram/users/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class ProfileForm(forms.Form):
     """Profile form."""
 
-    website = forms.URLField(max_length=200, required=True)
+    website = forms.URLField(max_length=200, required=False)
     biography = forms.CharField(max_length=500, required=False)
     phone_number = forms.CharField(max_length=20, required=False)
     picture = forms.ImageField()
