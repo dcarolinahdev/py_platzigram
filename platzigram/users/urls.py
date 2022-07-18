3f7e82069f2b3d5042a0f4f7445d8d87ea278071
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-07-18 18:32:11 -0500

    [Class based views] TemplateView for user detail - v1

--- platzigram/users/urls.py
+++ platzigram/users/urls.py
@@ -1,9 +1,20 @@
+# Django
 from django.urls import path
+# Views
+from django.views.generic import TemplateView
 from . import views
 
 app_name = "users"
 
 urlpatterns = [
+    # Posts
+    path(
+        route='<str:username>/',
+        view=TemplateView.as_view(template_name='users/detail.html'),
+        name='detail'
+    ),
+
+    # User management
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('signup/', views.signup_view, name='signup'),
