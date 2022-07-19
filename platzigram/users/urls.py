ad65636a430009edb9b68f2e4f198cbdeefd9bb9
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-07-19 17:51:42 -0500

    [URLs] Reordering URLs for users app

--- platzigram/users/urls.py
+++ platzigram/users/urls.py
@@ -6,17 +6,17 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
-    # Posts
-    path(
-        route='@<str:username>/',
-        view=views.UserDetailView.as_view(template_name='users/detail.html'),
-        name='detail'
-    ),
-
     # User management
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('signup/', views.SignupView.as_view(), name='signup'),
 
     path('me/profile/', views.update_profile, name='update_profile'),
+
+    # About posts user
+    path(
+        route='@<str:username>/',
+        view=views.UserDetailView.as_view(template_name='users/detail.html'),
+        name='detail'
+    ),
 ]
